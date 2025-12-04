78af390be53b64cbacfd00c991d83c1dfaa152a1
ShAhilK2 <shahilkataria852002@gmail.com>
2025-12-04 21:05:24 +0530

    fix : previous

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,35 +5,44 @@ import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { functions, inngest } from "./config/inngest.js";
 import { serve } from "inngest/express";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.json());
-app.use(clerkMiddleware());
+app.use(clerkMiddleware()); // add auth object under the req => req.auth
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
+
+
+
+
+
 app.get("/api/health", (req, res) => {
-  res.status(200).json({ message: "Just checking the health of the server : )" });
+  res
+    .status(200)
+    .json({ message: "Just checking the health of the server : )" });
 });
+
+// make our app ready for development
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../admin/dist/index.html"));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin"), "dist", "index.html");
   });
 }
 
 const startServer = async () => {
   await connectDB();
   app.listen(ENV.PORT, () => {
-    console.log(`Server running on port ${ENV.PORT}`);
+    console.log("Server is up and running");
   });
 };
+
+
 
 startServer();
